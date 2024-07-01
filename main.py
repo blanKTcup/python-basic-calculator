d215733344bb9eb705c656e703a7e654ef9292a8
blanKTcup <kthightran024@gmail.com>
2024-06-30 18:31:34 -0700

    added operation calculator function

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
 operation = input("Type your operation via it's corresponding number without the period: ")
+
+def addition(a, b):
+  return a + b
+
+def subtraction(a, b):
+  return a - b
+
+def multiplication(a, b):
+  return a * b
+
+def division(a, b):
+  return a / b
